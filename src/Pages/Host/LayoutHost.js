957f6422b7d076284c8d64a545cee080967c4697
Cristian-VA <132118380+Cristian-VA@users.noreply.github.com>
2023-06-22 22:48:28 -0500

    1 file modified

--- src/Pages/Host/LayoutHost.js
+++ src/Pages/Host/LayoutHost.js
@@ -4,6 +4,12 @@ import { Link, Outlet } from "react-router-dom"
 import styled from "styled-components" 
 
 const StyledDiv = styled.div`
+display:flex;
+justify-content: space-between;
+padding: 1.5em 1em;
+padding-right: 8em;
+color: rgba(77, 77, 77, 1);
+font-weight: 500;
 
 `
 
@@ -11,10 +17,10 @@ export default function LayoutHost (){
   return (
     <>
       <StyledDiv>
-         <Link  to="/host">Dashboard</Link>
-          <Link to="/host/income">Income</Link>
-          <Link to="/host/Vans">Vans</Link>
-          <Link to="/host/Reviews">Reviews</Link>
+         <Link className="hover:underline hover:text-gray-900"  to="/host">Dashboard</Link>
+          <Link className="hover:underline hover:text-gray-900" to="/host/income">Income</Link>
+          <Link className="hover:underline hover:text-gray-900" to="/host/Vans">Vans</Link>
+          <Link className="hover:underline hover:text-gray-900" to="/host/Reviews">Reviews</Link>
       </StyledDiv>
       <Outlet/>
    </>
